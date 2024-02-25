d468cbf89d1c10ad054585d2d3d90787c8f6acdd
Pisa12341 <160148246+Pisa12341@users.noreply.github.com>
2024-02-25 22:11:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,151 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Массив с текстом для создания div
-    var texts = [
-        "Пример текста 1",
-        "Пример текста 2",
-        "Пример текста 3",
-        "Пример текста 4",
-        "Пример текста 5"
+$(document).ready(function() {
+    const videos = [
+        'https://razgovor-cdn.edsoo.ru/media/video/russianscience-14-3.mp4',
+        'https://razgovor-cdn.edsoo.ru/media/video/russianscience-14-4.mp4',
+        'https://razgovor-cdn.edsoo.ru/media/video/russianscience-14-5.mp4'
     ];
 
-    // Находим контейнер, куда будем добавлять div
-    var container = document.querySelector(".container");
-
-    // Определяем высоту экрана
-    var screenHeight = window.innerHeight;
-
-    // Переменная для отслеживания начальной точки касания
+    var container = $(".container");
+    var screenHeight = $(window).height();
     var startY = 0;
-
-    // Переменная для хранения начальной позиции скролла
     var initialScrollPosition = 0;
-
-    // Переменная для хранения времени начала движения
     var startTime = 0;
-
-    // Переменная для хранения времени окончания движения
     var endTime = 0;
-
-    // Флаг для отслеживания прокрутки
     var isScrolling = false;
+    var currentVideo = null;
 
-    // Слушаем событие touchstart
-    container.addEventListener("touchstart", function(event) {
+    container.on("touchstart", function(event) {
         if (!isScrolling) {
             startY = event.touches[0].clientY;
-            initialScrollPosition = window.scrollY; // Сохраняем начальную позицию скролла
-            startTime = new Date().getTime(); // Сохраняем время начала движения
+            initialScrollPosition = window.scrollY;
+            startTime = new Date().getTime();
         }
     });
 
-    // Слушаем событие touchend
-    container.addEventListener("touchend", function(event) {
+    container.on("touchend", function(event) {
         if (!isScrolling) {
             var endY = event.changedTouches[0].clientY;
             var scrollDistance = startY - endY;
-            endTime = new Date().getTime(); // Сохраняем время окончания движения
+            endTime = new Date().getTime();
 
-            // Если пользователь промотал более чем на 40%
             if (Math.abs(scrollDistance) > screenHeight * 0.4) {
-                // Если пользователь прокрутил вниз
                 if (scrollDistance > 0) {
-                    // Вычисляем скорректированную высоту экрана
                     var adjustedHeight = screenHeight - Math.abs(scrollDistance);
-
-                    // Прокручиваем до следующего div
                     window.scrollBy(0, adjustedHeight);
                 } else {
-                    // Если пользователь прокрутил вверх более чем на 40%, привязываемся к верхнему div
                     var currentScroll = window.scrollY;
                     var index = Math.floor(currentScroll / screenHeight);
                     window.scrollTo(0, index * screenHeight);
                 }
             } else {
-                // Если пользователь прокрутил менее чем на 40%, оставляемся на текущем div
                 window.scrollTo(0, initialScrollPosition);
             }
 
-            // Проверяем скорость движения и прокручиваем на следующий div, если скорость достаточно высока
             var swipeDuration = endTime - startTime;
-            var swipeSpeed = Math.abs(scrollDistance) / swipeDuration; // Скорость в пикселях в миллисекунду
+            var swipeSpeed = Math.abs(scrollDistance) / swipeDuration;
 
             if (swipeSpeed > 0.5) {
-                // Если скорость выше порогового значения, прокручиваем на следующий div
                 if (scrollDistance > 0) {
-                    window.scrollBy(0, screenHeight); // Прокручиваем на следующий div
-                    isScrolling = true; // Устанавливаем флаг прокрутки
+                    window.scrollBy(0, screenHeight);
+                    isScrolling = true;
                     setTimeout(function() {
-                        isScrolling = false; // Сбрасываем флаг прокрутки через 500мс
+                        isScrolling = false;
                     }, 500);
                 }
             }
         }
     });
 
-    // Проходимся по массиву и создаем div с текстом
-    texts.forEach(function(text) {
-        var div = document.createElement("div");
-        div.textContent = text;
-        div.style.height = screenHeight + "px"; // Устанавливаем высоту div
-        container.appendChild(div);
+    container.on("touchmove", function(event) {
+        var touchY = event.touches[0].clientY;
+        var divs = $(".container > div");
+
+        divs.each(function() {
+            var rect = this.getBoundingClientRect();
+            if (touchY >= rect.top && touchY <= rect.bottom) {
+                var video = $(this).find("video")[0];
+                if (video && !video.paused && video !== currentVideo) {
+                    video.pause();
+                }
+            }
+        });
     });
+
+    container.on("dblclick", function(event) {
+        var video = currentVideo.find("video")[0];
+        if (video) {
+            video.play();
+            toggleLike(currentVideo);
+        }
+    });
+
+    videos.forEach(function(videoSrc) {
+        var div = $("<div></div>");
+        var video = $("<video></video>");
+        var pauseOverlay = $("<div class='pause-overlay'></div>");
+        var pauseIcon = $("<i class='pause-icon fas fa-pause'></i>");
+        var likeButton = $("<div class='like-button'></div>");
+
+        video.attr("src", videoSrc);
+        video.attr("loop", true);
+        video.attr("preload", "auto");
+        div.append(video);
+        div.append(pauseOverlay);
+        pauseOverlay.append(pauseIcon);
+        div.append(likeButton);
+        div.css("height", screenHeight + "px");
+        container.append(div);
+
+        div.on("click", function() {
+            var videosInViewport = $(".container > div video");
+            videosInViewport.each(function() {
+                if (this !== video[0]) {
+                    this.pause();
+                    this.currentTime = 0;
+                }
+            });
+
+            if (video[0].paused) {
+                video[0].play();
+                pauseOverlay.hide();
+            } else {
+                video[0].pause();
+                pauseOverlay.show();
+            }
+        });
+
+        video.on("timeupdate", function() {
+            if (video[0].currentTime > 0 && video[0].paused) {
+                pauseOverlay.show();
+            }
+        });
+
+        likeButton.on("click", function(event) {
+            event.stopPropagation();
+            toggleLike(div);
+        });
+
+        div.on("scroll", function() {
+            var rect = this.getBoundingClientRect();
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                currentVideo = $(this);
+                if (video[0].paused) {
+                    video[0].currentTime = 0;
+                    setTimeout(function() {
+                        video[0].play();
+                    }, 100);
+                }
+            } else {
+                if (currentVideo === $(this)) {
+                    currentVideo = null;
+                    video[0].pause();
+                }
+            }
+        });
+    });
+
+    function toggleLike(div) {
+        var likeButton = div.find(".like-button");
+        likeButton.toggleClass("like-active");
+    }
 });
